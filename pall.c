9719f12cacc8ea57afbe2ab99b5fd0c7efa33abf
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-01-22 20:29:25 -0800

    pall.c task

---
+++ pall.c
@@ -0,0 +1,19 @@
+/**
+* pall - Print all values in the stack
+* @stack: Doulble poineter to the top of the stack
+* @line_num: The line of the file the command was found
+*/
+
+void pall(stack_t **stack, unsigned int line_num)
+{
+	stack_t *walker;
+
+	(void) line_num;
+	walker = *stack;
+	while (walker != NULL)
+	{
+		printf("%d\n", walker->n);
+		walker = walker->next;
+	}
+}
+
