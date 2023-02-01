1ebd08293b2945103369dddb41c802871fe348b1
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-01 14:01:24 -0800

    pint task

---
+++ pint.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+* f_pint - prints the top
+* @head: stack head
+* @line_number: counter
+*
+* Return: None
+*/
+
+void f_pint(stack_t **head, unsigned int line_number)
+{
+	if (*head == NULL)
+	{
+		fprint(stderr, "L%u: can't pint, stack empty\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
