3b4cdc5f1c7429a324ca0e8d22f69b8ad716e9ef
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-01 13:48:12 -0800

    pall task corrected

--- pall.c
+++ pall.c
@@ -1,12 +1,17 @@
+#include "monty.h"
+
 /**
 * f_pall - Print all values in the stack
 * @stack: Doulble poineter to the top of the stack
 * @line_num: The line of the file the command was found
+*
+* Return: Null
 */
 
 void f_pall(stack_t **head, unsigned int line_number)
 {
 	stack_t *h;
+	(void) line_number;
 
 	h = *head;
 	if (h == NULL)
