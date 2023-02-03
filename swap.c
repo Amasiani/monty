585e3478aacdc6cbb3721daffee0dfa2c9f8b113
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-02 20:44:04 -0800

    swap file corrected

--- swap.c
+++ swap.c
@@ -8,7 +8,7 @@
 * Return: None
 */
 
-void f_swap(stack_t **head, unsigned int line_number)
+void f_swap(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
@@ -21,6 +21,14 @@ void f_swap(stack_t **head, unsigned int line_number)
 	}
 	if (len < 2)
 	{
-	
+		fprintf(stderr, "L%d: can't swap, stack to short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
 	}
+	h = *head;
+	aux = h->n;
+	h->n = h->next->n;
+	h->next->n = aux;
 }
