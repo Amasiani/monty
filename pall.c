45afefaaccf9dc2eaddd765c8ab82d99145ea58a
Amasiani Emeka joseph <amasianiemeka@gmail.com>
2023-02-02 22:13:26 -0800

    pall file corrected

--- pall.c
+++ pall.c
@@ -2,8 +2,8 @@
 
 /**
 * f_pall - Print all values in the stack
-* @stack: Doulble poineter to the top of the stack
-* @line_num: The line of the file the command was found
+* @head: Double pointer to the top of the stack
+* @line_number: The line of the file the command was found
 *
 * Return: Null
 */
@@ -11,7 +11,7 @@
 void f_pall(stack_t **head, unsigned int line_number)
 {
 	stack_t *h;
-	(void) line_number;
+	(void)line_number;
 
 	h = *head;
 	if (h == NULL)
